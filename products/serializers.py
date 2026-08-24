3c5a068628abe4d388491c3fa1cc87805c6b604b
veliyener <velirsg9@gmail.com>
2026-08-24 12:40:57 +0300

    serializer katmanı eklendi (sayfalama doğrulaması dahil)

---
+++ products/serializers.py
@@ -0,0 +1,19 @@
+from rest_framework import serializers
+from .models import Product
+
+
+class ProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = ['id', 'company_id', 'barcode', 'name', 'created_at', 'updated_at']
+        read_only_fields = ['id', 'created_at', 'updated_at']
+
+
+class ProductListQuerySerializer(serializers.Serializer):
+    page = serializers.IntegerField(required=False, default=1, min_value=1)
+    size = serializers.IntegerField(required=False, default=20, min_value=1)
+
+    def validate_size(self, value):
+        if value > 100:
+            raise serializers.ValidationError("size en fazla 100 olabilir.")
+        return value
