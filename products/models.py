cb3c84f5b623433f26a3023a972008f598706a09
veliyener <velirsg9@gmail.com>
2026-08-28 15:01:14 +0300

    Product modeline company_title alanı eklendi, migration Neon'a uygulandı

--- products/models.py
+++ products/models.py
@@ -5,6 +5,7 @@ from django.db import models
 class Product(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     company_id = models.UUIDField(db_index=True)
+    company_title = models.CharField(max_length=255, blank=True, default='')
     barcode = models.CharField(max_length=13)
     name = models.CharField(max_length=255)
     created_at = models.DateTimeField(auto_now_add=True)
