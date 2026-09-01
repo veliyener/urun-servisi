16a0dee19027a2f0200359542b2738b6f08df52d
veliyener <velirsg9@gmail.com>
2026-09-01 13:50:47 +0300

    servis katmanı için test eklendi: olmayan ürün id ile hata fırlaması

--- products/tests.py
+++ products/tests.py
@@ -1,3 +1,10 @@
-from django.test import TestCase
+import pytest
+from .services import ProductService, ProductNotFoundError
 
-# Create your tests here.
+
+@pytest.mark.django_db
+def test_olmayan_id_ile_urun_istendiginde_hata_firlar():
+    service = ProductService()
+
+    with pytest.raises(ProductNotFoundError):
+        service.get_product("00000000-0000-0000-0000-000000000000")
